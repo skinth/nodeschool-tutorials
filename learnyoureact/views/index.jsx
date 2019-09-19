5905c14026db8d85972ad64d4e7402d4d6c566f0
skinth <ste0891@gmail.com>
2019-09-19 10:18:20 +0200

    ex3-props

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 export default class TodoBox extends React.Component {
     render() {
         return (
-        <div className="todoBox">
-            <h1>Todos</h1>
-            <TodoList/>
-            <TodoForm/>
-        </div>
+            <div className="todoBox">
+                <h1>Todos</h1>
+                <TodoList />
+                <TodoForm />
+            </div>
         );
     }
 }
@@ -16,9 +16,25 @@ export default class TodoBox extends React.Component {
 class TodoList extends React.Component {
     render() {
         return (
-        <div className="todoList">
-            I am a TodoList.
-        </div>
+            <div className="todoList">
+                <table style={{ border: "2px solid black;" }}>
+                    <tbody>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+class Todo extends React.Component {
+    render() {
+        return (
+          <tr>
+            <td style={{border:'1px solid black;'}}>{this.props.title}</td>
+            <td style={{border:'1px solid black;'}}>{this.props.children}</td>
+          </tr>
         );
     }
 }
@@ -26,8 +42,8 @@ class TodoList extends React.Component {
 class TodoForm extends React.Component {
     render() {
         return (
-        <div className="todoForm">
-            I am a TodoForm.
+            <div className="todoForm">
+                I am a TodoForm.
         </div>
         );
     }
